0df3c6fdaaafca12affb2d3a6c0cb734282e2ee6
clarkgh <demolisho@hotmail.com>
2018-04-16 20:53:40 -0700

    removed let variable naming convention and changed to constants

--- src/App/scripts/fibbonaciSolver.js
+++ src/App/scripts/fibbonaciSolver.js
@@ -1,22 +1,22 @@
 'use strict';
 
-let multiplyMatrices = (...matrices) => {
+const multiplyMatrices = (...matrices) => {
   return (matrices.reduce( ([a, b, c], [d, e, f]) => {
     return [a * d + b * e, a * e + b * f, b * e + c * f];
   }));
 }
 
-let matrixExponentation = (matrix, n) => {
+const matrixExponentation = (matrix, n) => {
   if (n === 1) {
     return matrix;
   }
 
-  let halves = matrixExponentation(matrix, Math.floor(n / 2));
+  const halves = matrixExponentation(matrix, Math.floor(n / 2));
 
   return n % 2 === 0 ? multiplyMatrices(halves, halves) : multiplyMatrices(halves, halves, matrix);
 }
 
-let fibonacciSolver = (n) => {
+const fibonacciSolver = (n) => {
   if (n < 0 || n > 1476) {
     return "Please submit a positive integer below 1477.";
   }
